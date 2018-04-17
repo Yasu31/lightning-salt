41fb42d0a8c4e4b83b5a258ee3bcd138c12b65b6
Yasu <yasu313nori@gmail.com>
2018-04-17 21:28:29 +0900

    fix: copy and paste

--- test_bot.py
+++ test_bot.py
@@ -2,8 +2,8 @@ from xml.dom import minidom
 # https://qiita.com/akabei/items/38f974716f194afea4a5
 def load_credentials():
     xdoc=minidom.parse("credentials.xml")
-    token = xdoc.getElementsByTagName("token")[0].childNodes[0].data
-    secret=xdoc.getElementsByTagName("secret")[0].childNodes[0].data
+    token = "C63GEmcb4YrRrr7qx82/eJSSOxKpmHhXQo3cuoMzK2q7924z4IDBdFz82s1sqFZdg9lp2U/nNfxrRSqPukgHmMsw3HqyVCtHYmfqd3B/ogmjiYxuYhpTDIyDURx4IO1quQFAaR/9T0CdMDT86qIiQgdB04t89/1O/w1cDnyilFU="#xdoc.getElementsByTagName("token")[0].childNodes[0].data
+    secret= "a4604b99df8bf0df1c06ce0ce1f91a7"#xdoc.getElementsByTagName("secret")[0].childNodes[0].data
     return (token, secret)
 
 import os
@@ -24,8 +24,8 @@ from linebot.models import (
 app = Flask(__name__)
 
 # get channel_secret and channel_access_token from your environment variable
-channel_access_token, channel_secret =load_credentials()
-
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
@@ -64,5 +64,5 @@ def message_text(event):
 
 
 if __name__ == "__main__":
-    port=int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
