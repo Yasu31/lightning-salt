75b38680cfc4ee09827ec29cac7a8ba711fa7eed
Yasu <yasu313nori@gmail.com>
2018-04-17 21:13:07 +0900

    fix: edit based on https://qiita.com/akabei/items/38f974716f194afea4a5

--- test_bot.py
+++ test_bot.py
@@ -1,5 +1,5 @@
 from xml.dom import minidom
-
+# https://qiita.com/akabei/items/38f974716f194afea4a5
 def load_credentials():
     xdoc=minidom.parse("credentials.xml")
     token = xdoc.getElementsByTagName("token")[0].childNodes[0].data
@@ -64,11 +64,5 @@ def message_text(event):
 
 
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, port=options.port)
+    port=int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
