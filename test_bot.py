a1a7a02fd164ea7ac3ebe61c4d4484ca5e035953
Yasu <yasu313nori@gmail.com>
2018-04-17 22:04:01 +0900

    fix: delete reply

--- test_bot.py
+++ test_bot.py
@@ -45,9 +45,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text=event.message.text))
+    return
 
 
 if __name__ == "__main__":
