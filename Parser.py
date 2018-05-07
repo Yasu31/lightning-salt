e60c2f61c7ef7ea2c15153fc89f8e91d23cec1e0
Yasu <yasu313nori@gmail.com>
2018-05-07 23:31:29 +0900

    feat: Parser.py includes class to load CSV files

--- Parser.py
+++ Parser.py
@@ -1,18 +1,86 @@
 from pykakasi import kakasi
 import numpy as np
-'''
-uses the pykakasi library to convert Japanese text into hiragana, then change
-that into a series of one-hot vectors for inputting into an RNN.
-https://github.com/miurahr/pykakasi
-https://qiita.com/almichest/items/52f871ee22e4a44346d4
-'''
+import csv
+import os.path
 
+class FilesManager:
+    '''
+    loads and writes to files (such as CSV and parameters)
+    '''
+    def __init__(self):
+        pass
 
+    def load_dataset(self, botname="okan"):
+        '''
+        loads the csv file with datasets (named dataset_<botname>.csv, inside /csv directory)
+        ignores the first row.
+        ignores rows whose second columns are empty (i.e. unlabeled)
+        return as list where each line is a pair of a string and integer, like
+        [["Input sentense desu~~", 3], ["next input desu!", 1]]
+        '''
+        path = os.path.abspath("csv/dataset_"+botname+".csv")
+        dataset_raw = self.__load_csv(path)
+        dataset = []
+        # ignore the first line (it's a header row)
+        for line in dataset_raw[1:]:
+            if self.__check_int(line[1]):
+                # the second column is an integer
+                dataset.append([line[0], int(line[1])])
+            # otherwise, consider that line is not filled out properly and ignore.
+        return dataset
+
+    def __check_int(self, string):
+        '''
+        checks if a given string is an integer.
+        https://stackoverflow.com/questions/1265665/how-can-i-check-if-a-string-represents-an-int-without-using-try-except
+        '''
+        try:
+            int(string)
+            return True
+        except ValueError:
+            return False
+        
+    def load_phrases(self, botname="okan"):
+        '''
+        loads the csv file with phrases for the chatbot (named phrases_<botname>.csv, inside /csv directory)
+        the first row is ignored, as are rows whose first columns do not contain integers.
+        Returns as a dictionary.
+        the key is the integer index, and the value is a list of all the phrases in it.
+        '''
+        path = os.path.abspath("csv/phrases_"+botname+".csv")
+        phrases_raw = self.__load_csv(path)
+        phrases = {}
+        # ignore the first line
+        for line in phrases_raw[1:]:
+            if self.__check_int(line[0]):
+                dict = []
+                for item in line[1:]:
+                    if item != "":
+                        # if the element actually contains a phrase
+                        dict.append(item)
+                phrases[int(line[0])] = dict
+        return phrases
+        
+    def __load_csv(self, path):
+        '''
+        loads csv file, and returns it as a list
+        '''
+        print("opening file ", path)
+        list_to_return = []
+        with open(path) as csvfile:
+            reader = csv.reader(csvfile)
+            for line in reader:
+                list_to_return.append(line)
+        return list_to_return
+            
 class Parser:
     '''
     reads in a sentence in Japanese and outputs it as a vector.
+    uses the pykakasi library to convert Japanese text into hiragana, then change
+    that into a series of one-hot vectors for inputting into an RNN.
+    https://github.com/miurahr/pykakasi
+    https://qiita.com/almichest/items/52f871ee22e4a44346d4
     '''
-
     def __init__(self, debug=True):
         self.kakasi = kakasi()
         self.kakasi.setMode("H", "H")
@@ -72,3 +140,6 @@ class Parser:
 if __name__ == "__main__":
     parser = Parser()
     parser.sentence2vecs("今日は、いい天気なのかな？笑 m(_ _)m")
+    fm = FilesManager()
+    print(fm.load_dataset())
+    print(fm.load_phrases())
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # lightning-salt
 
-use the *ls-env* virtualenv
+use the *ls-env* virtualenv, using the requirements listed in *requirements.txt*
 
-Make sure that credentials.xml exists, because that holds the access tokens and such for the LINE account.
-
-To access the files in Heroku, `heroku run bash`.
+Make sure that credentials.xml exists, and that it holds the access tokens and such for the LINE account.
+```xml
+<!-- credentials.xml -->
+<data>
+  <token> *channel access token* </token>
+  <secret> *channel secret" </secret>
+</data>
+```
